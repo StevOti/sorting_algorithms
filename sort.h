ace54bac6d4f6242e1daafa05716b26dafba0ce4
StevOti <steveotieno5640@gmail.com>
2024-01-23 17:03:18 -0800

    Dealer

--- sort.h
+++ sort.h
@@ -65,4 +65,9 @@ void bitonic_sort(int *array, size_t size);
 int hoare_partition(int *array, size_t size, int left, int right);
 void hoare_sort(int *array, size_t size, int left, int right);
 void quick_sort_hoare(int *array, size_t size);
+int _strcmp(const char *s1, const char *s2);
+char get_value(deck_node_t *card);
+void insertion_sort_deck_kind(deck_node_t **deck);
+void insertion_sort_deck_value(deck_node_t **deck);
+void sort_deck(deck_node_t **deck);
 #endif
