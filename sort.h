f9c1acea0a652ed0509f0a1e093909cf92df67b0
StevOti <steveotieno5640@gmail.com>
2024-01-23 16:15:40 -0800

    Shell sort - Knuth Sequence

--- sort.h
+++ sort.h
@@ -45,4 +45,5 @@ void selection_sort(int *array, size_t size);
 int lomuto_partition(int *array, size_t size, int left, int right);
 void lomuto_sort(int *array, size_t size, int left, int right);
 void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
 #endif
